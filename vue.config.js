5d2880724142ea0a23781fee5cb43cead1a7fedc
christina <christina@delfsengineering.ca>
2022-06-03 20:21:04 +0000

    test - resolve directly

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,8 @@ const generateDevProjects = () => {
 
 module.exports = {
 	baseUrl: process.env.NODE_ENV === "production" ? "" : "/",
-	outputDir: process.env.NODE_ENV === "production" ? "dist" : path.resolve("dev/projects"),
+	//outputDir: process.env.NODE_ENV === "production" ? "dist" : path.resolve("dev/projects"),
+	outputDir: process.env.NODE_ENV === "production" ? path.resolve("../vue-form-generator") : path.resolve("dev/projects"),
 	lintOnSave: true,
 	runtimeCompiler: false,
 	transpileDependencies: [],
